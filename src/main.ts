0acc307cb9f625a731ddacda0fd91fdfc98f69a9
mulf <mail@mulf.wtf>
2025-04-08 16:15:39 -0500

    add root route handler

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,13 @@ const router = createRouter();
 app.use(router);
 
 router.get(
+    '/',
+    handler(() => {
+        return { message: 'hello' };
+    })
+);
+
+router.get(
     '/healthcheck',
     handler((evt) => {
         setResponseStatus(evt, 200);
