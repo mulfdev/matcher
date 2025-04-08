ad8728e08a0f12bfdf9be77ee2b0d489534a77c3
mulf <mail@mulf.wtf>
2025-04-08 16:13:31 -0500

    update handler format

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { createApp, toNodeListener, setResponseStatus } from 'h3';
+import {
+    createApp,
+    toNodeListener,
+    setResponseStatus,
+    createRouter,
+    defineEventHandler as handler,
+} from 'h3';
 import { createServer } from 'http';
 
 const app = createApp();
 
-app.use('/healthcheck', (evt) => {
-    setResponseStatus(evt, 200);
-    return { status: 'OK' };
-});
+const router = createRouter();
+app.use(router);
+
+router.get(
+    '/healthcheck',
+    handler((evt) => {
+        setResponseStatus(evt, 200);
+        return { status: 'OK' };
+    })
+);
 
 createServer(toNodeListener(app)).listen(3000, '0.0.0.0', () => {
     console.log('Server running on http://0.0.0.0:3000');
