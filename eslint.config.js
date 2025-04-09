46692fbbe83c11ffec6d915edbe3c5e5b43b688d
mulf <mail@mulf.wtf>
2025-04-09 15:58:53 -0500

    ignore dist

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,19 @@ import tseslint from 'typescript-eslint';
 import configPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
+    {
+        ignores: ['dist/**'],
+    },
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+
     configPrettier
 );
