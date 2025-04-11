ee711b2f4280a1184c9aade617a89dad2bd73f29
mulf <mail@mulf.wtf>
2025-04-11 17:07:17 -0500

    schema

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+export interface JobPostingsDetails {
+    id: string;
+    created_at: string;
+    job_posting_id?: number;
+    description?: string;
+    title?: string;
+    location?: string;
+    compensation?: string;
+    summary?: string;
+    job_postings_scraping_id?: string;
+    last_modified: string;
+    embedding?: number[];
+}
+
+declare module 'knex/types/tables.js' {
+    interface Tables {
+        job_postings_details: JobPostingsDetails;
+    }
+}
