1b7f881d995e1a6e6afdc22613b5010b4103faad
mulf <mail@mulf.wtf>
2025-04-12 16:30:32 -0500

    make scripts exec

--- dump-db/add-embedding-column.sh
+++ dump-db/add-embedding-column.sh
@@ -5,4 +5,4 @@ docker run -it --rm postgres psql \
   -p 14533 \
   -U mulf0 \
   -d jobs_rag \
-  -c "ALTER TABLE job_postings_details ADD COLUMN embedding vector(1536);"
+  -c "ALTER TABLE job_postings_details ADD COLUMN embeddings vector(1536);"
