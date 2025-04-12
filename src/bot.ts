30c1560cb876affd43ccf66ea00bec4ddf180ade
mulf <mail@mulf.wtf>
2025-04-12 12:39:45 -0500

    init simple agent and tool

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,6 @@
+import 'dotenv/config';
+import { agent, AgentStream, tool, openai, Settings } from 'llamaindex';
+import { z } from 'zod';
 import { Bot } from 'grammy';
 import got from 'got';
 import { ok } from 'assert';
@@ -6,7 +9,6 @@ import { createId } from '@paralleldrive/cuid2';
 import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import { readdir, readFile, rm } from 'fs/promises';
 import { join } from 'path';
-import { z } from 'zod';
 import { Poppler } from 'node-poppler';
 
 type MessageContent = { type: 'text'; text: string } | { type: 'image_url'; image_url: string };
@@ -89,41 +91,68 @@ bot.on(':document', async (ctx) => {
             })
         );
 
-        const textBlock: MessageContent[] = [
-            {
-                type: 'text',
-                text: 'classify this person based on their resume, i have included images of each page of their resume, ensure you understand the content, think step by step about the candidate, their skills and experience and tell me what you think of their skills',
-            },
-        ];
-        const contentArray = textBlock.concat(base64Images);
-
-        const body = {
+        // const textBlock: MessageContent[] = [
+        //     {
+        //         type: 'text',
+        //         text: 'classify this person based on their resume, i have included images of each page of their resume, ensure you understand the content, think step by step about the candidate, their skills and experience and tell me what you think of their skills',
+        //     },
+        // ];
+        // const contentArray = textBlock.concat(base64Images);
+        //
+        // const body = {
+        //     model: 'meta-llama/llama-4-maverick',
+        //     messages: [
+        //         {
+        //             role: 'user',
+        //             content: contentArray,
+        //         },
+        //     ],
+        //     max_tokens: 800,
+        //     temperature: 0.2,
+        //     top_p: 1,
+        //     presence_penalty: 0.1,
+        //     frequency_penalty: 0.3,
+        // };
+        //
+        // const response = await got.post('https://openrouter.ai/api/v1/chat/completions', {
+        //     headers: {
+        //         Authorization: `Bearer ${OPENROUTER_KEY}`,
+        //         'Content-Type': 'application/json',
+        //         'X-OpenRouter-Provider': 'lambda',
+        //     },
+        //     json: body,
+        //     responseType: 'json',
+        // });
+        //
+        // console.log(JSON.stringify(response.body, null, 2));
+        //
+        Settings.llm = openai({
+            apiKey: OPENROUTER_KEY,
             model: 'meta-llama/llama-4-maverick',
-            messages: [
-                {
-                    role: 'user',
-                    content: contentArray,
-                },
-            ],
-            max_tokens: 800,
-            temperature: 0.2,
-            top_p: 1,
-            presence_penalty: 0.1,
-            frequency_penalty: 0.3,
-        };
-
-        const response = await got.post('https://openrouter.ai/api/v1/chat/completions', {
-            headers: {
-                Authorization: `Bearer ${OPENROUTER_KEY}`,
-                'Content-Type': 'application/json',
-                'X-OpenRouter-Provider': 'lambda',
-            },
-            json: body,
-            responseType: 'json',
         });
 
-        console.log(JSON.stringify(response.body, null, 2));
+        const sumNumbers = ({ a, b }: { a: number; b: number }) => {
+            return `${a + b}`;
+        };
 
+        const addTool = tool({
+            name: 'sumNumbers',
+            description: 'Use this function to sum two numbers',
+            parameters: z.object({
+                a: z.number({
+                    description: 'First number to sum',
+                }),
+                b: z.number({
+                    description: 'Second number to sum',
+                }),
+            }),
+            execute: sumNumbers,
+        });
+
+        const myAgent = agent({ tools: [addTool] });
+        const context = myAgent.run('Sum 101 and 303');
+        const result = await context;
+        console.log(result.data);
         await rm(subDir, { recursive: true, force: true });
     } catch (e) {
         console.log(e);
