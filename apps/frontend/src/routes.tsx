1fef5d3c732e2c8009c1d4082f76651866506ab9
mulf <mail@mulf.wtf>
2025-05-05 18:52:19 -0500

    expand dashboard route

--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -2,8 +2,17 @@ import { createBrowserRouter } from 'react-router';
 import Login from '~/pages/login/Login';
 import Home from '~/pages/home/Home';
 import Dashboard from '~/pages/dashboard/Dashboard';
+import DashboardLayout from '~/layouts/DashboardLayout';
+import DashboardMatches from '~/pages/dashboard/Matches';
+
 export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: 'login', element: <Login /> },
-  { path: 'dashboard', element: <Dashboard /> },
-]);
+  {
+    path: "/dashboard",
+    Component: DashboardLayout,
+    children: [
+      { index: true, Component: Dashboard },
+      { path: "matches", Component: DashboardMatches }
+    ]
+  }])
