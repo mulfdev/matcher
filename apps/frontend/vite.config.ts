0c605f3579263be07aa26cbf84e9ba499732bf0b
mulf <mail@mulf.wtf>
2025-05-06 17:33:16 -0500

    chunk deps

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,4 +5,39 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('react-router')) {
+              return 'vendor-router';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-framer-motion';
+            }
+            if (id.includes('zustand')) {
+              return 'vendor-zustand';
+            }
+            if (id.includes('@tanstack/react-query')) {
+              return 'vendor-react-query';
+            }
+            if (id.includes('@headlessui') || id.includes('@heroicons')) {
+              return 'vendor-ui-kits';
+            }
+            if (id.includes('react-dropzone')) {
+              return 'vendor-react-dropzone';
+            }
+            if (id.includes('@react-oauth')) {
+              return 'vendor-react-oauth';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
