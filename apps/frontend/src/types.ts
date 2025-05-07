54de3b2487c73b4a620ef86568ed9ea1fd396fc1
mulf <mail@mulf.wtf>
2025-05-07 16:48:59 -0500

    add prop type

--- apps/frontend/src/types.ts
+++ apps/frontend/src/types.ts
@@ -1,1 +1,21 @@
 export type GoogleAuthRes = { email: string; name: string; userId: string };
+
+export type Skill = string;
+
+export type Experience = {
+  company: string;
+  duration_months: number;
+  end_date: string;
+  responsibilities: string[];
+  start_date: string;
+  title: string;
+};
+
+export type ResumeData = {
+  skills: Skill[];
+  experience: Experience[];
+  total_experience_years: number;
+  career_level: string;
+  category: string;
+  summary: string;
+};
