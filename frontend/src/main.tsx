b7db14ddbd39b894ec705e0bee495a045653c744
mulf <mail@mulf.wtf>
2025-05-29 15:03:34 -0500

    remove unused google dep

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { router } from '~/routes.tsx';
@@ -11,9 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId="324461339602-0j9u4t1d82vt1cu6ch5qesr48q3tt5c2.apps.googleusercontent.com">
-        <RouterProvider router={router} />
-      </GoogleOAuthProvider>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
